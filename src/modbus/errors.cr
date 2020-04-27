9953c18507187d84ef4aa5936a9b941f5e3c2ea8
Scott Nelson <scott@scottnelson.co>
2020-04-27 09:09:09 -0400

    Setup GH pages for docs

--- src/modbus/errors.cr
+++ src/modbus/errors.cr
@@ -1,13 +1,13 @@
 module Modbus
   EXCEPTIONS = {
-    1 => "Illegal Function",
-    2 => "Illegal Data Address",
-    3 => "Illegal Data Value",
-    4 => "Slave Device Failure",
-    5 => "Acknowledge",
-    6 => "Slave Device Busy",
-    7 => "Negative Acknowledge",
-    8 => "Memory Parity Error",
+     1 => "Illegal Function",
+     2 => "Illegal Data Address",
+     3 => "Illegal Data Value",
+     4 => "Slave Device Failure",
+     5 => "Acknowledge",
+     6 => "Slave Device Busy",
+     7 => "Negative Acknowledge",
+     8 => "Memory Parity Error",
     10 => "Gateway Path Unavailable",
     11 => "Gateway Target Device Failed to Respond",
   }
